58d504ea7697b436e1ce5be7b25d7c9391abad26
Alexei Petrovskiy <pas1-by@yandex.ru>
2019-03-19 13:02:30 +0300

    add AdvancedZeros1 task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let i = 0, div = 2, mas=[],repeat =[],power =[],result = [];
+
+    while (base > 1)
+   {
+       if(base % div == 0)
+       {
+           mas[i]= div;
+           repeat[i] = 0;
+           while (base % div == 0)
+           {
+               base = base / div;
+               repeat[i]+=1;
+           }
+           i++;
+       }
+       if (div == 2) div++;
+       else div += 2;
+   }
+ 
+    for(let j = 0; j < mas.length; j++) {
+         power[j] = maxPowerOf(mas[j], number);
+         result[j] = Math.floor(power[j]/repeat[j]);  
+    }
+ 
+    function compareNumeric(a, b) {
+         if (a > b) return 1;
+         if (a < b) return -1;
+     }
+     result.sort(compareNumeric);
+
+     return result[0];
+    
+ 
+    function  maxPowerOf(p, number)
+     {
+         let k = 0;
+         let powerOfP = 1;
+         while (true)
+         {
+             powerOfP *= p;
+             let add = Math.floor(number / powerOfP);
+             if (add == 0) {
+                 return k;
+             }
+             k+= add;
+         }
+     }
 }
